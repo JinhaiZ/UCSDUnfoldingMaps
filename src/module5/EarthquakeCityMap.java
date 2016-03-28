4c5d8aa0bf8872af569bcf5991646dd5d82ba759
jinhai <zhoujinhaixd@gmail.com>
2016-03-28 18:50:24 +0200

    complete selectMarkerIfHover method

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -146,6 +146,15 @@ public class EarthquakeCityMap extends PApplet {
 	private void selectMarkerIfHover(List<Marker> markers)
 	{
 		// TODO: Implement this method
+		for (Marker marker: markers) {
+			if (( marker.isInside(map, mouseX, mouseY))&& (lastSelected == null)) {
+				lastSelected.setClicked(true);
+				//execute only once then quit
+				lastSelected = (CommonMarker) marker;
+				marker.setSelected(true);
+				break;
+			}
+		}
 	}
 	
 	/** The event handler for mouse clicks
