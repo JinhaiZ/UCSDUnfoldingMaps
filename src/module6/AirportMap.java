c2904592c42a4a2291ec18563198bee6febea080
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-04-16 09:19:08 +0200

    add 3 providers of map

--- src/module6/AirportMap.java
+++ src/module6/AirportMap.java
@@ -10,6 +10,10 @@ import de.fhpotsdam.unfolding.data.ShapeFeature;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.Microsoft;
+import de.fhpotsdam.unfolding.providers.Yahoo;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import de.fhpotsdam.unfolding.geo.Location;
 import parsing.ParseFeed;
@@ -23,16 +27,28 @@ import processing.core.PApplet;
  */
 public class AirportMap extends PApplet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// The map
 	UnfoldingMap map;
+	AbstractMapProvider provider1;
+	AbstractMapProvider provider2;
+	AbstractMapProvider provider3;
 	private List<Marker> airportList;
 	List<Marker> routeList;
 	
 	public void setup() {
 		// setting up PAppler
-		size(800,600, OPENGL);
+		size(900,700, OPENGL);
+		provider1 = new Google.GoogleMapProvider();
+	    provider2 = new Microsoft.AerialProvider();
+	    provider3 = new Microsoft.HybridProvider() ;
+		
 		
 		// setting up map and default events
-		map = new UnfoldingMap(this, 50, 50, 750, 550);
+		map = new UnfoldingMap(this, 200, 50, 650, 600, provider1);
 		MapUtils.createDefaultEventDispatcher(this, map);
 		
 		// get features from airport data
@@ -72,7 +88,7 @@ public class AirportMap extends PApplet {
 			
 			SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
 		
-			System.out.println(sl.getProperties());
+			//System.out.println(sl.getProperties());
 			
 			//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
 			//routeList.add(sl);
@@ -83,13 +99,84 @@ public class AirportMap extends PApplet {
 		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
 		//map.addMarkers(routeList);
 		
-		map.addMarkers(airportList);
+		//map.addMarkers(airportList);
 		
 	}
 	
 	public void draw() {
 		background(0);
 		map.draw();
+		addKey();		
+	}
+	public void keyPressed() {
+	    if (key == '1') {
+	        map.mapDisplay.setProvider(provider1);
+	    } else if (key == '2') {
+	        map.mapDisplay.setProvider(provider2);
+	    } else if (key == '3') {
+	        map.mapDisplay.setProvider(provider3);
+	    }
+	}
+	private void addKey() {	
+		// Remember you can use Processing's graphics methods here
+		fill(255, 250, 240);
+		
+		int xbase = 25;
+		int ybase = 50;
+		
+		rect(xbase, ybase, 150, 250);
+		
+		fill(0);
+		textAlign(LEFT, CENTER);
+		textSize(12);
+		text("Earthquake Key", xbase+25, ybase+25);
+		
+		fill(150, 30, 30);
+		int tri_xbase = xbase + 35;
+		int tri_ybase = ybase + 50;
+		triangle(tri_xbase, tri_ybase-CityMarker.TRI_SIZE, tri_xbase-CityMarker.TRI_SIZE, 
+				tri_ybase+CityMarker.TRI_SIZE, tri_xbase+CityMarker.TRI_SIZE, 
+				tri_ybase+CityMarker.TRI_SIZE);
+
+		fill(0, 0, 0);
+		textAlign(LEFT, CENTER);
+		text("City Marker", tri_xbase + 15, tri_ybase);
+		
+		text("Land Quake", xbase+50, ybase+70);
+		text("Ocean Quake", xbase+50, ybase+90);
+		text("Size ~ Magnitude", xbase+25, ybase+110);
+		
+		fill(255, 255, 255);
+		ellipse(xbase+35, 
+				ybase+70, 
+				10, 
+				10);
+		rect(xbase+35-5, ybase+90-5, 10, 10);
+		
+		fill(color(255, 255, 0));
+		ellipse(xbase+35, ybase+140, 12, 12);
+		fill(color(0, 0, 255));
+		ellipse(xbase+35, ybase+160, 12, 12);
+		fill(color(255, 0, 0));
+		ellipse(xbase+35, ybase+180, 12, 12);
+		
+		textAlign(LEFT, CENTER);
+		fill(0, 0, 0);
+		text("Shallow", xbase+50, ybase+140);
+		text("Intermediate", xbase+50, ybase+160);
+		text("Deep", xbase+50, ybase+180);
+
+		text("Past hour", xbase+50, ybase+200);
+		
+		fill(255, 255, 255);
+		int centerx = xbase+35;
+		int centery = ybase+200;
+		ellipse(centerx, centery, 12, 12);
+
+		strokeWeight(2);
+		line(centerx-8, centery-8, centerx+8, centery+8);
+		line(centerx-8, centery+8, centerx+8, centery-8);
+		
 		
 	}
 	
